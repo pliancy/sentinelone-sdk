79c692c248f5349e2125629a48ecf660b443d732
Santese Smith <43831817+santese@users.noreply.github.com>
2024-05-22 22:51:50 -0700

    feat: add timeout and update depends

--- src/lib/utils/create-http-agent.ts
+++ src/lib/utils/create-http-agent.ts
@@ -4,7 +4,7 @@ import { SentinelOneConfig } from '../sentinel-one.types'
 export function createHttpAgent(config: SentinelOneConfig): AxiosInstance {
     const baseURL = `https://${config.site}/web/api/v2.1`
     const Authorization = `ApiToken ${config.token}`
-    const agent = axios.create({ baseURL })
+    const agent = axios.create({ baseURL, timeout: config.timeoutMs })
     agent.defaults.headers.common['Authorization'] = Authorization
 
     // On Axios error, throw a simple, relevant error
